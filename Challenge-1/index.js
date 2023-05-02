b36b76854f2f8e413c62d11c10535cbefac1f939
Arif Rahman <arif.rahman.rhm@gmail.com>
2023-05-02 21:09:03 +0700

    Feat: Challenge 1

---
+++ Challenge-1/index.js
@@ -0,0 +1,11 @@
+function printPattern() {
+  for (let i = 1; i <= 5; i++) {
+    let row = "";
+    for (let j = 1; j <= 5; j++) {
+      row += (i * j) + " ";
+    }
+    console.log(row);
+  }
+}
+
+printPattern();
