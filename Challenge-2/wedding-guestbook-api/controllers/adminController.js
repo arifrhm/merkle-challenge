746cd21b065458079b8fd60656473029f18d3673
Arif Rahman <arif.rahman.rhm@gmail.com>
2023-05-02 22:51:41 +0700

    Fix : Config error

--- Challenge-2/wedding-guestbook-api/controllers/adminController.js
+++ Challenge-2/wedding-guestbook-api/controllers/adminController.js
@@ -1,43 +1,100 @@
-const express = require('express');
-const router = express.Router();
-
+const jwt = require('jsonwebtoken');
 const Guest = require('../models/Guest');
+const Note = Guest;
 
-// Admin page - GET /admin
-router.get('/', async (req, res) => {
-  try {
-    const guests = await Guest.find({});
-    res.status(200).json({
-      status: 'success',
-      results: guests.length,
-      data: {
-        guests,
-      },
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 'error',
-      message: 'Internal server error',
-    });
+// Helper function to create JWT token
+function createToken(user) {
+  return jwt.sign({ id: user.id, email: user.email }, config.secret, {
+    expiresIn: 86400 // expires in 24 hours
+  });
+}
+
+module.exports = {
+  login: async (req, res) => {
+    try {
+      // Check if email and password are provided
+      if (!req.body.email || !req.body.password) {
+        return res.status(400).send({ message: 'Email and password are required' });
+      }
+
+      // Check if user exists and password matches
+      const user = await Admin.findOne({ email: req.body.email });
+      if (!user || !user.comparePassword(req.body.password)) {
+        return res.status(401).send({ message: 'Invalid email or password' });
+      }
+
+      // Create JWT token
+      const token = createToken(user);
+
+      // Return token and user details
+      res.status(200).send({ token, user: user.toJSON() });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ message: 'Internal server error' });
+    }
+  },
+
+  getAllGuests: async (req, res) => {
+    try {
+      // Get all guests from database
+      const guests = await Guest.find();
+
+      // Remove sensitive information from each guest object
+      const guestsWithoutSensitiveInfo = guests.map((guest) => {
+        const { _id, name, note } = guest;
+        return { _id, name, note };
+      });
+
+      // Return guests data
+      res.status(200).send(guestsWithoutSensitiveInfo);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ message: 'Internal server error' });
+    }
+  },
+
+  getAllNotes: async (req, res) => {
+    try {
+      // Get all notes from database
+      const notes = await Note.find();
+
+      // Remove sensitive information from each note object
+      const notesWithoutSensitiveInfo = notes.map((note) => {
+        const { _id, name } = note;
+        return { _id, name };
+      });
+
+      // Return notes data
+      res.status(200).send(notesWithoutSensitiveInfo);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ message: 'Internal server error' });
+    }
+  },
+
+  deleteGuest: async (req, res) => {
+    try {
+      // Find guest by id and remove from database
+      await Guest.findByIdAndRemove(req.params.id);
+
+      // Return success message
+      res.status(200).send({ message: 'Guest successfully deleted' });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ message: 'Internal server error' });
+    }
+  },
+
+  deleteNote: async (req, res) => {
+    try {
+      // Find note by id and remove from database
+      await Note.findByIdAndRemove(req.params.id);
+
+      // Return success message
+      res.status(200).send({ message: 'Note successfully deleted' });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ message: 'Internal server error' });
+    }
   }
-});
-
-// Delete entry - DELETE /admin/:id
-router.delete('/:id', async (req, res) => {
-  try {
-    const deletedGuest = await Guest.findByIdAndDelete(req.params.id);
-    res.status(200).json({
-      status: 'success',
-      data: {
-        guest: deletedGuest,
-      },
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 'error',
-      message: 'Internal server error',
-    });
-  }
-});
-
-module.exports = router;
+};
--- Challenge-2/wedding-guestbook-api/controllers/guestController.js
+++ Challenge-2/wedding-guestbook-api/controllers/guestController.js
@@ -1,43 +1,38 @@
-const express = require('express');
-const router = express.Router();
+// guestController.js
 
+// Import the Guest model
 const Guest = require('../models/Guest');
 
-// Guest Forms - POST /guests
-router.post('/', async (req, res) => {
+// Define the addGuest function
+exports.addGuest = async (req, res, next) => {
   try {
-    const newGuest = await Guest.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      data: {
-        guest: newGuest,
-      },
+    // Create a new guest object from the request body
+    const newGuest = new Guest({
+      name: req.body.name,
+      address: req.body.address,
+      phone: req.body.phone,
+      note: req.body.note
     });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err.message,
-    });
-  }
-});
 
-// Note Gallery - GET /guests
-router.get('/', async (req, res) => {
+    // Save the new guest object to the database
+    const savedGuest = await newGuest.save();
+
+    // Send a response with the saved guest object
+    res.status(201).json(savedGuest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Define the getAllGuests function
+exports.getAllGuests = async (req, res, next) => {
   try {
-    const guests = await Guest.find({}, { name: 1, note: 1, _id: 0 });
-    res.status(200).json({
-      status: 'success',
-      results: guests.length,
-      data: {
-        guests,
-      },
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 'error',
-      message: 'Internal server error',
-    });
-  }
-});
+    // Find all guests in the database
+    const guests = await Guest.find();
 
-module.exports = router;
+    // Send a response with the guest array
+    res.status(200).json(guests);
+  } catch (error) {
+    next(error);
+  }
+};
